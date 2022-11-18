31fdfa0fe43b3b5396cfffc032b1ec7c6ae3f7ce
iskry <refocuslucifer@protonmail.com>
2022-11-17 21:26:12 -0600

    added questions

--- index.js
+++ index.js
@@ -3,15 +3,47 @@ const genMarkdown = require("./utils/generateMarkdown")
 const inquirer = require("inquirer")
 const fs = require("fs")
 
+inquirer.prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What is the title of your project?"
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Provide a description of your project"
+    },
+    {
+      name: "installation",
+      type: "input",
+      message: "What installation steps are necessary to run your project?"
+    },
+    {
+      name: "contribute",
+      type: "input",
+      message: "How can users contribute to this project?"
+    },
+    {
+      name: "license",
+      type: "input",
+      message: "What license does your project use? (unsure? visit https://www.choosealicense.com)"
+    },
+    {
+      name: "project",
+      type: "input",
+      message: "What is the relative path to an image of your project?"
+    },
+  ])
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
